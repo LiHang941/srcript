5b62ab7ae1e3497b18bdc5fe12e0397eef718cad
LiHang <1329075357@qq.com>
2018-11-02 21:34:02 +0800

    Create 96xxzfl.js

---
+++ 96xxzfl.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         96xxzfl.com宅福利自动下一页
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       lihang1329@gmail.com
+// @include      *https://96xxzfl.com*
+// @supportURL https://jq.qq.com/?_wv=1027&k=5Y328gF
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    if(!/https:\/\/96xxzfl.com\/.+\/\d+\.html(.*?)/.test(window.location.href)){
+       return;
+       }
+
+    $(function(){
+       $(".pagination-multi li a").each(function(){
+           if($(this).html()=="下一页"){
+               next($(this).attr('href'));
+               $(".pagination-multi ").remove();
+           }
+       });
+    });
+
+    function next(url){
+        console.log(url);
+         $.get(url,function(res){
+             var nextReg = /<li class='next-page'><a href='(.+)'>(.+)<\/a><\/li>/g;
+             var reg =/<img.+src="(.+)"\s+\/>\s*<\/p>/g;
+             var match = reg.exec(res);
+             while (match != null) {
+                 $(".article-content p").last().after('<p><img src="'+match[1]+'"></p>');
+                 console.log(match[2]);
+                 match =  reg.exec(res);
+             }
+             match = nextReg.exec(res);
+             if(match != null) {
+                 next(match[1]);
+             }
+         },"text");
+    }
+})();
