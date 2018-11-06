97ed3cea34b895c3d20b089fb2f60d4574aadbd6
LiHang <1329075357@qq.com>
2018-11-06 22:02:37 +0800

    增加左右键滚动，看起来更爽

--- 96xxzfl.js
+++ 96xxzfl.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         96xxzfl.com宅福利自动下一页
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       lihang1329@gmail.com
 // @include      *https://96xxzfl.com*
@@ -12,33 +12,43 @@
 
 (function() {
     if(!/https:\/\/96xxzfl.com\/.+\/\d+\.html(.*?)/.test(window.location.href)){
-       return;
-       }
+        return;
+    }
 
     $(function(){
-       $(".pagination-multi li a").each(function(){
-           if($(this).html()=="下一页"){
-               next($(this).attr('href'));
-               $(".pagination-multi ").remove();
-           }
-       });
+        $(".pagination-multi li a").each(function(){
+            if($(this).html()=="下一页"){
+                next($(this).attr('href'));
+            }
+        });
+
+        var flag = false;
+        $(window).keydown(function(event){
+            var y = window.screen.availHeight;
+            if(event.keyCode === 37 ){
+                y = $(window).scrollTop() - y;
+            }else if(event.keyCode === 39){
+                y = $(window).scrollTop() + y;
+            } else {return;}
+            $("html,body").scrollTop(y);
+        });
     });
 
     function next(url){
         console.log(url);
-         $.get(url,function(res){
-             var nextReg = /<li class='next-page'><a href='(.+)'>(.+)<\/a><\/li>/g;
-             var reg =/<img.+src="(.+)"\s+\/>\s*<\/p>/g;
-             var match = reg.exec(res);
-             while (match != null) {
-                 $(".article-content p").last().after('<p><img src="'+match[1]+'"></p>');
-                 console.log(match[2]);
-                 match =  reg.exec(res);
-             }
-             match = nextReg.exec(res);
-             if(match != null) {
-                 next(match[1]);
-             }
-         },"text");
+        $.get(url,function(res){
+            var nextReg = /<li class='next-page'><a href='(.+)'>(.+)<\/a><\/li>/g;
+            var reg =/<img.+src="(.+)"\s+\/>\s*<\/p>/g;
+            var match = reg.exec(res);
+            while (match != null) {
+                $(".article-content p").last().after('<p><img src="'+match[1]+'"></p>');
+                console.log(match[2]);
+                match =  reg.exec(res);
+            }
+            match = nextReg.exec(res);
+            if(match != null) {
+                next(match[1]);
+            }
+        },"text");
     }
 })();
